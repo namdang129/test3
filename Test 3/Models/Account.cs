ef2e1b30e2f3b10c64b88cd841c93cbfca1c6102
SonSWE <dangtiensonuh@gmail.com>
2021-09-23 07:30:41 +0700

    push ojbect

---
+++ Test 3/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Test2.Models
+{
+    public partial class Account
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Test 3/Models/TestAspContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Test2.Models
+{
+    public partial class TestAspContext : DbContext
+    {
+        public TestAspContext()
+        {
+        }
+
+        public TestAspContext(DbContextOptions<TestAspContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=TestAsp;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.HasKey(e => e.UserName);
+
+                entity.ToTable("Account");
+
+                entity.Property(e => e.UserName).HasMaxLength(100);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
